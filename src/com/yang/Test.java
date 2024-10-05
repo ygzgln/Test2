82e46a6361301d32cd9696c598ff3895e221fba4
ygzgln <ygzgln@163.com>
2024-10-05 08:46:14 +0800

    第一次提交

---
+++ src/com/yang/Test.java
@@ -0,0 +1,12 @@
+package com.yang;
+
+/**
+ * @author Zhong.Yl
+ * 2024/10/5 8:45
+ * Test2
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
